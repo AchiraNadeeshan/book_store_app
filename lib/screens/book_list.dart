b5ef620b6c945aba126ade2c4e803e90b8b13bc8
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-01-20 17:32:21 +0530

    Craeted the book details screen

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -1,50 +1,135 @@
 import 'package:flutter/material.dart';
-import '../widgets/book.dart';
+import 'book_details.dart';
 
 /// Booklist widget
-/// Uses the [Book] widget to display a list of books.
+/// Displays a list of books and navigates to their details page on tap.
 class BookList extends StatelessWidget {
   const BookList({super.key});
+
+  /// Navigates to the book details page.
+  void navigateToBookDetails(
+    BuildContext context,
+    String title,
+    String author,
+    String coverImagePath,
+    double price,
+  ) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => BookDetails(
+          title: title,
+          author: author,
+          coverImagePath: coverImagePath,
+          price: price,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ceylon Bookstore'),
-        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'Ceylon Bookstore',
+          style: TextStyle(color: Colors.white), // White text color
+        ),
+        backgroundColor: Colors.blue[900], // Dark blue background color
       ),
       body: SafeArea(
         child: ListView(
-          children: const [
-            Book(
-              coverImagePath: 'assets/images/rich-dad-poor-dad.jpg',
-              title: 'Rich Dad, Poor Dad',
-              author: 'Robert T. Kiyosaki',
-              price: 1250,
+          children: [
+            GestureDetector(
+              onTap: () => navigateToBookDetails(
+                context,
+                'Rich Dad, Poor Dad',
+                'Robert T. Kiyosaki',
+                'assets/images/rich-dad-poor-dad.jpg',
+                1250,
+              ),
+              child: const ListTile(
+                leading: Image(
+                  image: AssetImage('assets/images/rich-dad-poor-dad.jpg'),
+                  height: 50,
+                ),
+                title: Text('Rich Dad, Poor Dad'),
+                subtitle: Text('Robert T. Kiyosaki'),
+                trailing: Text('LKR 1250'),
+              ),
             ),
-            Book(
-              coverImagePath: 'assets/images/the-alchemist.jpg',
-              title: 'The Alchemist',
-              author: 'Paulo Coelho',
-              price: 1000,
+            GestureDetector(
+              onTap: () => navigateToBookDetails(
+                context,
+                'The Alchemist',
+                'Paulo Coelho',
+                'assets/images/the-alchemist.jpg',
+                1000,
+              ),
+              child: const ListTile(
+                leading: Image(
+                  image: AssetImage('assets/images/the-alchemist.jpg'),
+                  height: 50,
+                ),
+                title: Text('The Alchemist'),
+                subtitle: Text('Paulo Coelho'),
+                trailing: Text('LKR 1000'),
+              ),
             ),
-            Book(
-              coverImagePath: 'assets/images/men-are-from-mars.jpg',
-              title: 'Men Are From Mars',
-              author: 'John Gray',
-              price: 1500,
+            GestureDetector(
+              onTap: () => navigateToBookDetails(
+                context,
+                'Men Are From Mars',
+                'John Gray',
+                'assets/images/men-are-from-mars.jpg',
+                1500,
+              ),
+              child: const ListTile(
+                leading: Image(
+                  image: AssetImage('assets/images/men-are-from-mars.jpg'),
+                  height: 50,
+                ),
+                title: Text('Men Are From Mars'),
+                subtitle: Text('John Gray'),
+                trailing: Text('LKR 1500'),
+              ),
             ),
-            Book(
-              coverImagePath: 'assets/images/who-killed-change.jpg',
-              title: 'Who Killed Change?',
-              author: 'Ken Blanchard',
-              price: 900,
+            GestureDetector(
+              onTap: () => navigateToBookDetails(
+                context,
+                'Who Killed Change?',
+                'Ken Blanchard',
+                'assets/images/who-killed-change.jpg',
+                900,
+              ),
+              child: const ListTile(
+                leading: Image(
+                  image: AssetImage('assets/images/who-killed-change.jpg'),
+                  height: 50,
+                ),
+                title: Text('Who Killed Change?'),
+                subtitle: Text('Ken Blanchard'),
+                trailing: Text('LKR 900'),
+              ),
             ),
-            Book(
-              coverImagePath: 'assets/images/the-doomsday-conspiracy.jpg',
-              title: 'The Doomsday Conspiracy',
-              author: 'Sidney Sheldon',
-              price: 1100,
+            GestureDetector(
+              onTap: () => navigateToBookDetails(
+                context,
+                'The Doomsday Conspiracy',
+                'Sidney Sheldon',
+                'assets/images/the-doomsday-conspiracy.jpg',
+                1100,
+              ),
+              child: const ListTile(
+                leading: Image(
+                  image:
+                      AssetImage('assets/images/the-doomsday-conspiracy.jpg'),
+                  height: 50,
+                ),
+                title: Text('The Doomsday Conspiracy'),
+                subtitle: Text('Sidney Sheldon'),
+                trailing: Text('LKR 1100'),
+              ),
             ),
           ],
         ),
